746f70aafff38e6878324952ab38c2b0752dba22
Chris Maloney <voldrani@gmail.com>
2018-02-20 08:49:56 -0500

    Same, but defines a NotGate class

--- src/script.js
+++ src/script.js
@@ -4,24 +4,43 @@ function log(msg) {
 
 var draw = SVG('drawing').size(700, 350);
 
-var NotGate = {
-  x: 200,
-  y: 100
-};
 
-drawNotGate(NotGate);
+class NotGate {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    draw.circle()
+      .attr({'class': 'notGate'})
+      .radius(6)
+      .attr({cx: this.x+22, cy: this.y});
+    draw.polygon('' +
+        (this.x-28) + ',' + (this.y-30) + ' ' +
+        (this.x-28) + ',' + (this.y+30) + ' ' +
+        (this.x+16) + ',' + (this.y))
+      .attr({'class': 'notGate'});
+  }
+  getInputPoint() {
+    return [this.x - 28, this.y];
+  }
+}
+
+var notGate = new NotGate(200, 100);
+notGate.draw();
+
 
 function drawWireToNot (from, gate){
   draw.polyline([from, [gate.x-28, gate.y]])
     .attr({'class': 'wire'});
 }
-drawWireToNot([100, 100], NotGate)
+drawWireToNot([100, 100], notGate)
 
 function drawWireFromNot(gate, to) {
   var points = [];
   var x0 = gate.x + 28;
   points.push([x0, gate.y]);
-  
+
   if (points[0][1] != to[1]) {
     points.push([(to[0]-x0)/2+points[0][0], points[0][1]]);
     points.push([(to[0]-x0)/2+points[0][0], to[1]] );
@@ -30,21 +49,4 @@ function drawWireFromNot(gate, to) {
   draw.polyline(points)
     .attr({'class': 'wire'});
 }
-drawWireFromNot(NotGate, [500, 200]);
-
-
-
-
-//-------------------------------------
-function drawNotGate(gate) {
-  draw.circle()
-    .attr({'class': 'notGate'})
-    .radius(6)
-    .attr({cx: gate.x+22, cy: gate.y});
-  draw.polygon('' +
-    (gate.x-28) + ',' + (gate.y-30) + ' ' + 
-    (gate.x-28) + ',' + (gate.y+30) + ' ' +
-    (gate.x+16) + ',' + (gate.y))
-    .attr({'class': 'notGate'});
-}
-
+drawWireFromNot(notGate, [500, 200]);
