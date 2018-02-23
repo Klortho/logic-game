965d163f6dca910a0d14253e1ccddda4c861c7c2
Chris Maloney <voldrani@gmail.com>
2018-02-22 22:07:51 -0500

    lesson 3 - or gate

--- src/script.js
+++ src/script.js
@@ -15,17 +15,11 @@ class NotGate {
     this.polygon = draw.polygon([[x + 16, y], [x - 28, y + 30], [x - 28, y - 30]])
       .attr({fill: 'none', 'stroke-width': 3, stroke: 'red'});
   }
-  draw() {
-    var x = this.x;
-    var y = this.y;
-    this.circle.move(x + 16, y - 6)
-    this.polygon.plot([[x + 16, y], [x - 28, y + 30], [x - 28, y - 30]])
-  }
   outputPos() {
-    return [this.x + 28, this.y]
+    return [this.x + 28, this.y];
   }
   inputPos(){
-    return [this.x - 28, this.y]
+    return [this.x - 28, this.y];
   }
 }
 
@@ -36,50 +30,76 @@ class AndGate {
 
     const svg = document.getElementsByTagName('svg')[0];
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute('d', 'M ' + (x - 2) + ',' + (y - 30) + ' h -26 v 60 h 26 a 30 30 0 0 0 0 -60');
+    path.setAttribute('d', 'M ' + (x - 2) + ',' + (y - 30) +
+                      ' h -26 v 60 h 26 a 30 30 0 0 0 0 -60');
     path.setAttribute('fill', 'none');
     path.setAttribute('stroke', 'green');
     path.setAttribute('stroke-width', 3);
     svg.appendChild(path);
   }
-  draw() {
-    var x = this.x;
-    var y = this.y;
-    this.circle.move(x + 16, y - 6)
-    this.polygon.plot([[x + 16, y], [x - 28, y + 30], [x - 28, y - 30]])
+  outputPos() {
+    return [this.x + 28, this.y];
+  }
+  inputPos(inputNum) {
+    var y;
+    if (inputNum === 0) y = this.y - 15;
+    else y = this.y + 15;
+    return [this.x - 28, y];
+  }
+}
+
+class OrGate {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+
+    const svg = document.getElementsByTagName('svg')[0];
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const d = 'M ' + (x - 47.5) + ',' + (y - 30) + ' ' +
+              'h 32.5 ' +
+              'c 25,0 43,20 47.5,30 ' +
+              'c -4.5,10 -22.5,30 -47.5,30 ' +
+              'h -32.5 ' +
+              'a 60 60 0 0 0 0 -60';
+    path.setAttribute('d', d);
+    path.setAttribute('fill', 'none');
+    path.setAttribute('stroke', 'orange');
+    path.setAttribute('stroke-width', 3);
+    svg.appendChild(path);
   }
   outputPos() {
-    return [this.x + 28, this.y]
+    return [this.x + 32.5, this.y];
   }
-  inputPos(){
-    return [this.x - 28, this.y]
+  inputPos(inputNum) {
+    var y;
+    if (inputNum === 0) y = this.y - 15;
+    else y = this.y + 15;
+    return [this.x - 42, y];
   }
 }
 
 class Wire {
-  constructor(fromGate, toGate){
+  constructor(fromGate, toGate, inputNum) {
     this.fromGate = fromGate;
     this.toGate = toGate;
-    var start = fromGate.outputPos()
-    var end = toGate.inputPos()
-    var points = [start]
+    var start = fromGate.outputPos();
+    var end = toGate.inputPos(inputNum);
+    var points = [start];
     if(start[1] != end[1]){
-      points.push([(start[0] + end[0]) / 2, start[1]])
-      points.push([(start[0] + end[0]) / 2, end[1]])
+      points.push([(start[0] + end[0]) / 2, start[1]]);
+      points.push([(start[0] + end[0]) / 2, end[1]]);
     }
-    points.push(end)
+    points.push(end);
     this.wire = draw.polyline(points)
     .attr({stroke: 'blue', 'stroke-width': 2, fill: 'none'});
   }
 }
 
-var andGate = new AndGate(100, 100);
-
-
-var notGate = new NotGate(700,100);
-
-var notGate2 = new NotGate(1000,150)
-
-const wire = new Wire(notGate, notGate2)
-
-notGate.draw();
+var notGate0 = new NotGate(100,100);
+var notGate1 = new NotGate(200,150);
+var notGate2 = new NotGate(500,150);
+var andGate = new AndGate(300,300);
+var orGate = new OrGate(300,100);
+const wire0 = new Wire(notGate0, orGate, 0);
+const wire1 = new Wire(notGate1, andGate, 0);
+const wire2 = new Wire(orGate, notGate2);
